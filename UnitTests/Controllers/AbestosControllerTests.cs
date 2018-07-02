7343a530fa4bc6cb07938fb831f1b2493f0eb3ba
Miguel Saitz <miguel.saitz@hackney.gov.uk>
2018-07-02 14:52:25 +0100

    tests re-added to asbestos controller tests

--- UnitTests/Controllers/AbestosControllerTests.cs
+++ UnitTests/Controllers/AbestosControllerTests.cs
@@ -12,7 +12,61 @@ namespace UnitTests
 {
     public class AbestosControllerTests
     {
-        
+        [Fact]
+        public async Task return_200_for_valid_request()
+        {
+            var fakeResponse = new List<Inspection>();
+
+            var fakeAsbestosService = new Mock<IAsbestosService>();
+            fakeAsbestosService
+                .Setup(m => m.GetInspection(It.IsAny<string>()))
+                .Returns(Task.FromResult<IEnumerable<Inspection>>(fakeResponse));
+
+            var controller = new AsbestosController(fakeAsbestosService.Object);
+
+            var response = await controller.GetInspection("12345678");
+            Assert.Equal(200, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("123456678")]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData("1")]
+        [InlineData("123456789")]
+        [InlineData("ABC")]
+        [InlineData("A1234567")]
+        [InlineData("?1234567")]
+        public async Task return_400_for_invalid_request(string id)
+        {
+            var fakeResponse = new List<Inspection>();
+            var fakeAsbestosService = new Mock<IAsbestosService>();
+            fakeAsbestosService
+                .Setup(m => m.GetInspection(It.IsAny<string>()))
+                .Returns(Task.FromResult<IEnumerable<Inspection>>(fakeResponse));
+
+            var controller = new AsbestosController(fakeAsbestosService.Object);
+            var response = await controller.GetInspection(id);
+
+            Assert.Equal(400, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task return_404_if_request_is_successful_but_no_results()
+        {
+            var fakeResponse = new List<Inspection>();
+            var fakeAsbestosService = new Mock<IAsbestosService>();
+            fakeAsbestosService
+                .Setup(m => m.GetInspection(It.IsAny<string>()))
+                .Returns(Task.FromResult<IEnumerable<Inspection>>(fakeResponse));
+
+            var controller = new AsbestosController(fakeAsbestosService.Object);
+            var response = await controller.GetInspection("00000000");
+
+            Assert.Equal(404, response.StatusCode);
+        }
+
         [Fact]
         public async Task response_has_valid_content_if_request_successful()
         {
