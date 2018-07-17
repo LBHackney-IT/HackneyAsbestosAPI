3d3a78f773a9797509bd230e1893793cf1ab69d0
Miguel Saitz <miguel.saitz@hackney.gov.uk>
2018-07-17 13:08:12 +0100

    Added integration tests

--- LBHAsbestosAPI/Validators/IdValidator.cs
+++ LBHAsbestosAPI/Validators/IdValidator.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Linq;
-using LBHAsbestosAPI.Interfaces;
 
 namespace LBHAsbestosAPI.Validators
 {
     public static class IdValidator
     {
-       
         public static bool ValidatePropertyId(string propertyId)
         {
             // TODO current validation constrains are temporal and have yet to be reviewed 
