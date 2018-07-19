45224b39405c04b8f45d0e7a35d3fb19c61e3b92
LBHmsaitz <40232240+LBHmsaitz@users.noreply.github.com>
2018-07-19 09:39:04 +0100

    Fixed indentation issue #3

--- LBHAsbestosAPI/Entities/RoomResponse.cs
+++ LBHAsbestosAPI/Entities/RoomResponse.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace LBHAsbestosAPI.Entities
 {
     public class RoomResponse
     {
-		public bool Success { get; set; }
+	public bool Success { get; set; }
         public string ErrorMesage { get; set; }
         public Room Data { get; set; }
     }
