5ed48fee5fd56c9b787a4fd6cc1b89bd7b9cdb68
Miguel Saitz <miguel.saitz@hackney.gov.uk>
2018-07-24 14:11:19 +0100

    Services tests refactored

--- UnitTests/Services/AsbestosServicesTests.cs
+++ UnitTests/Services/AsbestosServicesTests.cs
@@ -22,6 +22,7 @@ namespace UnitTests.Services
         {
             fakeLogger = new Mock<ILoggerAdapter<AsbestosService>>();
             fakePsiLogger = new Mock<ILoggerAdapter<Psi2000Api>>();
+            TestStatus.IsRunningTests = false;
         }
 
         [Fact]
@@ -29,118 +30,82 @@ namespace UnitTests.Services
         {
             IEnumerable<Inspection> responseData;
 
-            // Case for the test running isolated from the other tests
-            // repository is mocked in this function
-            if (!TestStatus.IsRunningTests)
+            var fakeRepository = new Mock<IPsi2000Api>();
+            var fakeInspectionResponse = new InspectionResponse()
             {
-                var fakeRepository = new Mock<IPsi2000Api>();
-                var fakeInspectionResponse = new InspectionResponse()
-                {
-                    Data = new List<Inspection>()
-                };
+                Data = new List<Inspection>()
+            };
 
-                fakeInspectionResponse.Data.Add(new Inspection()
-                {
-                    Id = 655,
-                    LocationDescription = "A house"
-                });
-
-                fakeRepository
-                    .Setup(m => m.GetInspections(It.IsAny<string>()))
-                    .Returns(Task.FromResult(fakeInspectionResponse)); 
-
-                asbestosService = new AsbestosService(fakeRepository.Object, fakeLogger.Object);
-            }
-            // Case for the test running in conjunction with the solution tests.
-            // The asbestos service uses the fakePsi2000Api repository.
-            else
+            fakeInspectionResponse.Data.Add(new Inspection()
             {
-                asbestosService = new AsbestosService(new Psi2000Api(fakePsiLogger.Object), fakeLogger.Object);
-            }
+                Id = 6555,
+                LocationDescription = "A houser"
+            });
 
+            fakeRepository
+                .Setup(m => m.GetInspections(It.IsAny<string>()))
+                .Returns(Task.FromResult(fakeInspectionResponse)); 
+
+            asbestosService = new AsbestosService(fakeRepository.Object, fakeLogger.Object);
             responseData = await asbestosService.GetInspection("random string");
 
-            Assert.Equal(655, responseData.ElementAt(0).Id);
-            Assert.Equal("A house", responseData.ElementAt(0).LocationDescription);
+            Assert.Equal(6555, responseData.ElementAt(0).Id);
+            Assert.Equal("A houser", responseData.ElementAt(0).LocationDescription);
+        }
+
+        [Fact]
+        public async Task can_access_floor_data_from_response()
+        {
+            Floor responseData;
+            var fakeRepository = new Mock<IPsi2000Api>();
+            var fakeFloorResponse = new FloorResponse()
+            {
+                Data = new Floor()
+            };
+
+            fakeFloorResponse.Data = new Floor()
+            {
+                Id = 34344,
+                Description = "Firsto Floor"
+            };
+
+            fakeRepository
+                .Setup(m => m.GetFloor(It.IsAny<string>()))
+                .Returns(Task.FromResult(fakeFloorResponse));
+
+            asbestosService = new AsbestosService(fakeRepository.Object, fakeLogger.Object);
+            responseData = await asbestosService.GetFloor("random string");
+
+            Assert.Equal(34344, responseData.Id);
+            Assert.Equal("Firsto Floor", responseData.Description);
         }
 
         [Fact]
         public async Task can_access_room_data_from_response()
         {
             Room responseData;
-
-            // Case fof the test running isolated from the other tests
-            // repository is mocked in this function
-            if (!TestStatus.IsRunningTests)
+            var fakeRepository = new Mock<IPsi2000Api>();
+            var fakeRoomResponse = new RoomResponse()
             {
-                var fakeRepository = new Mock<IPsi2000Api>();
-                var fakeRoomResponse = new RoomResponse()
-                {
-                    Data = new Room()
-                };
+                Data = new Room()
+            };
 
-                fakeRoomResponse.Data = new Room()
-                {
-                    Id = 5003,
-                    Description = "Ground Floor"
-                };
-
-                fakeRepository
-                    .Setup(m => m.GetRoom(It.IsAny<string>()))
-                    .Returns(Task.FromResult(fakeRoomResponse));
-
-                asbestosService = new AsbestosService(fakeRepository.Object, fakeLogger.Object);
-            }
-            // Case for the test running in conjunction with the solution tests.
-            // The asbestos service uses the fakePsi2000Api repository.
-            else
+            fakeRoomResponse.Data = new Room()
             {
-                asbestosService = new AsbestosService(new Psi2000Api(fakePsiLogger.Object), fakeLogger.Object);
-            }
+                Id = 50034,
+                Description = "Ground Floor 1"
+            };
 
+            fakeRepository
+                .Setup(m => m.GetRoom(It.IsAny<string>()))
+                .Returns(Task.FromResult(fakeRoomResponse));
+            bool test = TestStatus.IsRunningTests;
+            TestStatus.IsRunningTests = false;
+            asbestosService = new AsbestosService(fakeRepository.Object, fakeLogger.Object);
             responseData = await asbestosService.GetRoom("random string");
-            Assert.Equal(5003, responseData.Id);
-            Assert.Equal("Ground Floor", responseData.Description);
-        }
 
-        //TODO review this test
-        [Fact]
-        public async Task can_access_floor_data_from_response()
-        {
-            Floor responseData;
-
-            // Case fof the test running isolated from the other tests
-            // repository is mocked in this function
-            if (!TestStatus.IsRunningTests)
-            {
-                var fakeRepository = new Mock<IPsi2000Api>();
-                var fakeFloorResponse = new FloorResponse()
-                {
-                    Data = new Floor()
-                };
-
-                fakeFloorResponse.Data = new Floor()
-                {
-                    Id = 3434,
-                    Description = "First Floor"
-                };
-
-                fakeRepository
-                    .Setup(m => m.GetFloor(It.IsAny<string>()))
-                    .Returns(Task.FromResult(fakeFloorResponse));
-
-                asbestosService = new AsbestosService(fakeRepository.Object, fakeLogger.Object);
-            }
-            // Case for the test running in conjunction with the solution tests.
-            // The asbestos service uses the fakePsi2000Api repository.
-            else
-            {
-                asbestosService = new AsbestosService(new Psi2000Api(fakePsiLogger.Object), fakeLogger.Object);
-            }
-
-            responseData = await asbestosService.GetFloor("random string");
-            Assert.Equal(3434, responseData.Id);
-            Assert.Equal("First Floor", responseData.Description);
+            Assert.Equal(50034, responseData.Id);
+            Assert.Equal("Ground Floor 1", responseData.Description);
         }
     }
 }
