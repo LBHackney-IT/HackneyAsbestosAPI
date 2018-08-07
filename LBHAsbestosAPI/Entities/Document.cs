ed041c9aee5d20b8d41d592f6232c5cf54b44ad7
Miguel Saitz <miguel.saitz@hackney.gov.uk>
2018-08-07 12:24:56 +0100

    add GetDocument() in repo and fix types in Document entity

--- LBHAsbestosAPI/Entities/Document.cs
+++ LBHAsbestosAPI/Entities/Document.cs
@@ -6,13 +6,13 @@ namespace LBHAsbestosAPI.Entities
 		public int Id { get; set; }
         public string Reference { get; set; }
         public string Description { get; set; }
-        public int JobId { get; set; } 
+        public int? JobId { get; set; } 
         public int PropertyId { get; set; } 
         public string Uprn { get; set; } 
         public string CreatedBy { get; set; } 
         public string ModifiedBy { get; set; } 
-        public string DateOfModification { get; set; }
-        public string DateOfCreation { get; set; } 
+        public DateTime? DateOfModification { get; set; }
+        public DateTime DateOfCreation { get; set; } 
         public bool IsApproved { get; set; }
     }
 }
